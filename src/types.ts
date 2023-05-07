2d153716de68cc41f47caeb46a97e7cd4c12997d
flukejaja <rockliverow90@hotmail.com>
2023-05-08 00:15:28 +0700

    add types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,13 @@
-export interface body_data {
+export interface body_type {
     id: number,
     first_name: string,
     last_name: string,
     email: string,
     gender: string,
     ip_address: string,
+}
+export interface response_data<T> {
+    message:string,
+    code: number,
+    data?:T
 }
